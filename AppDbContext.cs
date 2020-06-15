5dba66b0fb89e5d4ea8df7a2beefc85715635287
Sruthi Vasudevan <58195474+sruvats@users.noreply.github.com>
2020-06-14 22:27:30 -0500

    Added source code from local

---
+++ AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using apiControls.Models;
+using System.Data.Entity;
+
+namespace apiControls
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Customer> Customers {get;set;}
+        public DbSet<Movie> Movies {get;set;}
+        public DbSet<Rental> Rental { get; set; }
+
+        public AppDbContext()
+           : base("name=defaultconnection")
+        { }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="apiControls.WebApiApplication" Language="C#" %>
